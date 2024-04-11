e009848eddb33dfe22be2038d659bd0977ce15d5
Pedro Oliveira <012pedropaulo@gmail.com>
2024-04-11 12:37:39 -0300

    a

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         API_URL: "https://66155453b8b8e32ffc7a9bf2.mockapi.io",
-        DOMAIN_ORIGIN: "http://localhost:3000",
+        DOMAIN_ORIGIN: "http://127.0.0.1:3000",
     },
 }
 
